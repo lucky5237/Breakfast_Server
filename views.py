6a6c814572a7d621d9c00c3f4dcf36491905398b
jianlu <815093244@qq.com>
2016-04-07 20:17:13 +0800

    路由

---
+++ views.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from app import api
+from API.User import Login, Register, CheckMobile, ChangePwd, AveScore, UserInfo
+from API.Order import MakeOrder, OrderList, Comment, TakeOrder, UpdateStatus
+from API.Food import SalesRank, PriceRank
+
+# 用户相关路由
+api.add_resource(Login, '/user/login')
+api.add_resource(Register, '/user/register')
+api.add_resource(CheckMobile, '/user/checkMobile')
+api.add_resource(ChangePwd, '/user/changePassword')
+api.add_resource(AveScore, '/user/aveScore')
+api.add_resource(UserInfo, '/user/userInfo')
+
+# 订单相关路由
+api.add_resource(MakeOrder, '/order/makeOrder')
+api.add_resource(OrderList, '/order/orderList')
+api.add_resource(Comment, '/order/comment')
+api.add_resource(TakeOrder, '/order/takeOrder')
+api.add_resource(UpdateStatus, '/order/updateStatus')
+# 食物相关路由
+api.add_resource(SalesRank, '/food/salesRank')
+api.add_resource(PriceRank, '/food/priceRank')
